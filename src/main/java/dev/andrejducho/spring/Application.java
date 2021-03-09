f8d74a0ea98e09dd236e569a7a40b8476be8b1c0
duchyyy <duchoslavandrej@gmail.com>
2021-03-09 07:24:36 +0100

    i will show 3 techniques in next commits

---
+++ src/main/java/dev/andrejducho/spring/Application.java
@@ -0,0 +1,16 @@
+package dev.andrejducho.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
